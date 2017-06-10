6abaa622f4aa5afe7766601b878e3acc1c5380ce
ThreadedLemon <slade205@gmail.com>
2017-06-10 12:23:53 -0700

    log window locat add buildpacl

--- src/index.js
+++ src/index.js
@@ -23,8 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => { 
-  console.log(req);
-  console.log(res);
+  console.log(window.location);
   res.send('\n 👋 🌍 Test \n') 
 })
 
