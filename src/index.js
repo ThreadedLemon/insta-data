64a17fc57e8930e21edc633e7815d5a9040145fa
ThreadedLemon <slade205@gmail.com>
2017-06-10 12:08:17 -0700

    token get handler

--- src/index.js
+++ src/index.js
@@ -1,48 +1,53 @@
 
 'use strict'
 
-const express = require('express')
-const proxy = require('express-http-proxy')
-const bodyParser = require('body-parser')
-const _ = require('lodash')
-const config = require('./config')
-const commands = require('./commands')
-const helpCommand = require('./commands/help')
+const express = require('express');
+const proxy = require('express-http-proxy');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const config = require('./config');
+const commands = require('./commands');
+const helpCommand = require('./commands/help');
 
-let bot = require('./bot')
+let bot = require('./bot');
 
-let app = express()
+let app = express();
 
 if (config('PROXY_URI')) {
   app.use(proxy(config('PROXY_URI'), {
     forwardPath: (req, res) => { return require('url').parse(req.url).path }
   }))
-}
+};
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => { res.send('\n 👋 🌍 Test \n') })
 
 app.post('/commands/unfollow-list', (req, res) => {
-  let payload = req.body
+  let payload = req.body;
 
   console.log(payload);
 
   if (!payload || payload.token !== config('SLACK_TOKEN')) {
     let err = '✋  Insta—what? An invalid slash token was provided\n' +
-              '   Is your Slack slash token correctly configured?'
-    console.log(err)
-    res.status(401).end(err)
-    return
+              '   Is your Slack slash token correctly configured?';
+    console.log(err);
+    res.status(401).end(err);
+    return;
   }
 
   let cmd = _.reduce(commands, (a, cmd) => {
     return payload.text.match(cmd.pattern) ? cmd : a
-  }, helpCommand)
+  }, helpCommand);
 
-  cmd.handler(payload, res)
-})
+  cmd.handler(payload, res);
+});
+
+app.get('/#access_token', (req, res) => {
+  console.log(req);
+  console.log(res);
+});
 
 app.listen(config('PORT'), (err) => {
   if (err) throw err
@@ -53,4 +58,4 @@ app.listen(config('PORT'), (err) => {
     console.log(`🤖  beep boop: @starbot is real-time\n`)
     bot.listen({ token: config('SLACK_TOKEN') })
   }
-})
+});
