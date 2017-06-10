7e7f6ef5e91a6c33303dd3ff691600b4b0a1fae3
ThreadedLemon <slade205@gmail.com>
2017-06-10 13:27:21 -0700

    buildpack

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => { 
-  console.log(window.location);
 
   res.send('\n 👋 🌍 Test \n') 
 })
