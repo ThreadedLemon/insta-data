065dd18bdfc13e5e4924d05c2cb1bbdf65881ed1
ThreadedLemon <slade205@gmail.com>
2017-06-10 18:51:18 -0700

    update utilities

--- src/utilities.js
+++ src/utilities.js
@@ -4,6 +4,7 @@
  * @author    Slade Solobay
  * @date      2017-06-10 
  */
+const config = require('./config');
 const Horseman = require('node-horseman');
 const url = require('url');
 
@@ -19,7 +20,7 @@ module.exports = {
 
         horseman.open(`https://api.instagram.com/oauth/authorize/?client_id=eb2a475895d74b7fb0611dfd918e99c2&redirect_uri=https://insta-data.herokuapp.com/&response_type=token`)
             .on('urlChanged', (targetUrl) => {
-                config('INSTAGRAM_TOKEN', url.parse(targetUrl).hash.slice(1));
+                config('INSTAGRAM_TOKEN', url.parse(targetUrl).hash.slice(14));
                 console.log(config('INSTAGRAM_TOKEN'));
             })
             .value('input[name="username"]', 'integrationuser')
