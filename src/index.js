3c30dd0494171394f646968fec3eb3e62dc25174
ThreadedLemon <slade205@gmail.com>
2017-06-11 21:31:51 -0700

    boom

--- src/index.js
+++ src/index.js
@@ -66,8 +66,7 @@ app.post('/commands/engagement', (req, res) => {
   slackCallouts.message('Generating report...', payload.channel_id);
   slackCallouts.getHistory(payload.channel_id);
 
-  res.set('content-type', 'application/json');
-  res.status(200);
+  res.send(200);
   return;
 });
 
